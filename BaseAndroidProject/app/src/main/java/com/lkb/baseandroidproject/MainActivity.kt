3e85e1c138d4e5728386f8d08a6d782c4b42e4d8
Behera <inlbehera@ad.harman.com>
2018-12-10 16:41:49 +0530

    rx hello world added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,11 +2,26 @@ package com.lkb.baseandroidproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.function.Consumer
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = ""
+        // producer
+        var observable = Observable.just("hello", "hi","there")
+
+        // consumer
+        val consumer = this::updateUi
+
+        observable.subscribe(consumer)
+    }
+
+    fun updateUi(string:String){
+        textView.text = string;
     }
 }
