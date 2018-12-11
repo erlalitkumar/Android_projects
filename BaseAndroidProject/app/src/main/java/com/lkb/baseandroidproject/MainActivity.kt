7d0470381df0d5f73ec567b8457028f52c7fa8bb
Behera <inlbehera@ad.harman.com>
2018-12-11 12:14:03 +0530

    retrofit call added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,6 +3,9 @@ package com.lkb.baseandroidproject
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -11,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     lateinit var mViewModel: MainViewModel
     lateinit var disposable: Disposable
+    lateinit var wikiDisposable: Disposable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,25 @@ class MainActivity : AppCompatActivity() {
             .subscribe {
                 textView.text = it.toString();
             }
+
+        val clickListener = View.OnClickListener{
+            when(it.id){
+                R.id.searchButton->{
+                    val searchTextString = searchText.text.toString()
+                    wikiDisposable = mViewModel.callHitcountWikiApi(searchTextString)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                            { result -> hitCountText.text = "${result.query.searchinfo.totalhits} result found" },
+                            { error -> Log.i("error", error.toString())//Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                }
+            }
+        }
+        searchButton.setOnClickListener(clickListener)
+
+
 
     }
 
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainViewModel.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainViewModel.kt
@@ -4,8 +4,17 @@ import android.arch.lifecycle.ViewModel
 import io.reactivex.Observable
 
 class MainViewModel : ViewModel() {
+
+    private val wikiApiService by lazy {
+        WikiApiService.create()
+    }
+
     fun getData(): Observable<String> {
         return Observable.just("hello", "hi", "there")
+    }
+
+    fun callHitcountWikiApi(searchString:String):Observable<WikiModel.Result>{
+        return wikiApiService.hitCountCheck("query", "json", "search", searchString)
     }
 
 }
