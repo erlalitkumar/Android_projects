d34ba5f73107839fb3113c322d43817e9f09ec7d
Lalit Behera <lalit143.er@gmail.com>
2018-12-24 20:13:07 +0530

    External method imported and linked to Android view using data baonding

---
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/Util.kt
@@ -0,0 +1,10 @@
+package com.lkb.baseandroidproject
+
+class Util {
+    companion object {
+        @JvmStatic
+        fun capitalize(text:String): String {
+            return text.toUpperCase()
+        }
+    }
+}
