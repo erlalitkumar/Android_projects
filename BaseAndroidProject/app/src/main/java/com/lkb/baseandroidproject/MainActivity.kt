e1f7325e7b12b7b4072a70d5f4812f8c35faeed8
Lalit Behera <lalit143.er@gmail.com>
2018-12-24 12:01:24 +0530

    Databinding: click listener added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.lkb.baseandroidproject
 
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.lkb.baseandroidproject.databinding.ActivityMainBinding
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var handlers: MyClickHandlers
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -14,5 +19,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar);
         val usr = User("Lalit", "lalitkbehera@gmail.com")
         binding.user = usr
+        handlers = MyClickHandlers(this)
+        binding.handlers = handlers
+    }
+
+
+    inner class MyClickHandlers(internal var context: Context) {
+
+        fun onFabClicked(view: View) {
+            Toast.makeText(applicationContext, "FAB clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonClick(view: View) {
+            Toast.makeText(applicationContext, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonClickWithParam(view: View, user: User) {
+            Toast.makeText(applicationContext, "Button clicked! Name: " + user.name, Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonLongPressed(view: View): Boolean {
+            Toast.makeText(applicationContext, "Button long pressed!", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
