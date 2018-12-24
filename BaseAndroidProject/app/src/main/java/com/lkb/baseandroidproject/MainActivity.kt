b807c7bbab8013deaa6e8eae025a88c582ee8bb3
Lalit Behera <lalit143.er@gmail.com>
2018-12-24 09:38:21 +0530

    initial data binding project added with simple user pojo binding

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.lkb.baseandroidproject
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.lkb.baseandroidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val mainActivity: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val usr = User("Lalit", "lalitkbehera@gmail.com")
+        mainActivity.user = usr
     }
 }
---
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/User.kt
@@ -0,0 +1,3 @@
+package com.lkb.baseandroidproject
+
+data class User(val name:String, val email:String)
