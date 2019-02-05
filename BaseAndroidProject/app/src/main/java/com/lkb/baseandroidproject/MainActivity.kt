12bb436173fd6cc1c905346399197bca7b528afc
Behera <inlbehera@ad.harman.com>
2019-02-05 21:29:27 +0530

    fragment demo added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,11 +2,18 @@ package com.lkb.baseandroidproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fm = supportFragmentManager
+        val ft= fm.beginTransaction()
+        ft.add(R.id.containerOne,FragmentOne())
+        ft.add(R.id.containerTwo,FragmentTwo())
+        ft.commit()
     }
 }
