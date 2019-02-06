10772906127847ec89d8aeff67fa6967b2184a0c
Lalit Behera <lalit143.er@gmail.com>
2019-02-06 16:30:24 +0530

    recycler view added code done

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/RecyclerViewActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/RecyclerViewActivity.kt
@@ -3,17 +3,26 @@ package com.lkb.baseandroidproject
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import kotlinx.android.synthetic.main.create_user_layout.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
 import kotlinx.android.synthetic.main.recycler_main.*
 
 class RecyclerViewActivity : AppCompatActivity() {
     private val tag = "RecyclerViewActivity"
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recycler_main)
+        recyclerView = findViewById(R.id.my_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val a = arrayListOf<User>(User("Lalit", "Behera", "Lalit@gmail.com"))
+        adapter = RecyclerViewAdapter()
+        recyclerView.adapter = adapter
+        adapter.bindData(a)
         fab.setOnClickListener {
-            var intent = Intent(this@RecyclerViewActivity,UserCreateActivity::class.java)
+            var intent = Intent(this@RecyclerViewActivity, UserCreateActivity::class.java)
             startActivity(intent)
         }
     }
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/RecyclerViewAdapter.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/RecyclerViewAdapter.kt
@@ -1,28 +1,36 @@
-//package com.lkb.baseandroidproject
-//
-//import android.support.v7.widget.RecyclerView
-//import android.view.View
-//import android.view.ViewGroup
-//
-//class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
-//    var data: ArrayList<User> = arrayListOf()
-//    override fun onCreateViewHolder(parent: ViewGroup, postion: Int): MyViewHolder {
-//return MyViewHolder()
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return data.size
-//    }
-//
-//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//
-//    }
-//
-//    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//    }
-//
-//    fun bindData(data: ArrayList<User>) {
-//        this.data = data
-//    }
-//}
+package com.lkb.baseandroidproject
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+    var data: ArrayList<User> = arrayListOf()
+    override fun onCreateViewHolder(parent: ViewGroup, postion: Int): MyViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.firstName.text = data.get(position).firstName
+        holder.lastName.text = data.get(position).lastName
+        holder.email.text = data.get(position).email
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var firstName: TextView = itemView.findViewById(R.id.firstName)
+        var lastName: TextView = itemView.findViewById(R.id.lastName)
+        var email: TextView = itemView.findViewById(R.id.email)
+    }
+
+    fun bindData(data: ArrayList<User>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+}
