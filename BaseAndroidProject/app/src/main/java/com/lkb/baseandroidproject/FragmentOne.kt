20ad68a3d2cd59b9eabc4f67acac9d186c768939
Behera <inlbehera@ad.harman.com>
2019-02-06 11:39:32 +0530

    fragment communication added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/FragmentOne.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/FragmentOne.kt
@@ -1,15 +1,31 @@
 package com.lkb.baseandroidproject
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_one.*
 
-class FragmentOne: Fragment(){
+class FragmentOne : Fragment() {
+    lateinit var listener: OnButtonClickListener
+    fun setOnButtonClickListenr(activity: Activity) {
+        listener = activity as MainActivity
+    }
+
+    interface OnButtonClickListener {
+        fun buttonClicked(data:String)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_one,container,false)
+        val fragmentView = inflater.inflate(R.layout.fragment_one, container, false)
+        val buttonOne:Button = fragmentView.findViewById(R.id.button)
+        buttonOne.setOnClickListener {
+            listener.buttonClicked("Button one Clicked")
+        }
+        return fragmentView
     }
 
 }
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.lkb.baseandroidproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentManager
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), FragmentOne.OnButtonClickListener{
+
+    override fun buttonClicked(data:String) {
+        // delegate the event to the other fragment.
+        onButtonClickedInFragment(data)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fm = supportFragmentManager
-        val ft= fm.beginTransaction()
-        ft.add(R.id.containerOne,FragmentOne())
-        ft.add(R.id.containerTwo,FragmentTwo())
-        ft.commit()
+        if(savedInstanceState==null){
+            val fm = supportFragmentManager
+            val ft= fm.beginTransaction()
+            ft.add(R.id.containerOne,FragmentOne(),"ONE")
+            ft.add(R.id.containerTwo,FragmentTwo(),"TWO")
+            ft.addToBackStack(null)
+            ft.commit()
+        }
+    }
+
+    override fun onAttachFragment(fragment: Fragment?) {
+        if (fragment is FragmentOne) {
+            val headlinesFragment = fragment
+           headlinesFragment.setOnButtonClickListenr(this)
+        }
+    }
+    fun onButtonClickedInFragment(data:String){
+       val fragmentTwo:FragmentTwo? = supportFragmentManager.findFragmentByTag("TWO") as FragmentTwo?
+        if(fragmentTwo!=null){
+            fragmentTwo.updateUI(data)
+        }else{
+            // crate te fragment and set the data in a bundle.
+        }
     }
 }
