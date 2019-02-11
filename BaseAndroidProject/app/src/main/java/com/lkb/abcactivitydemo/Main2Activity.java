fdb181bd4b3e6601df9101fff8e055eb7a2a8094
Lalit Behera <lalit143.er@gmail.com>
2019-02-11 19:00:31 +0530

    if you call finish on onCreate method what will happen oncreate done then ondestroy

--- BaseAndroidProject/app/src/main/java/com/lkb/abcactivitydemo/Main2Activity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/abcactivitydemo/Main2Activity.java
@@ -3,6 +3,7 @@ package com.lkb.abcactivitydemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,5 +24,31 @@ public class Main2Activity extends AppCompatActivity {
                // finish();
             }
         });
+        finish();
+        Log.d("Activity2","oncreate end");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Activity2","onresume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Activity2","onStop called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Activity2","onpause called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Activity2","onDestroy called");
     }
 }
