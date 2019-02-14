0a301465bc142d97b581f3e1df33f03eb3add2d9
Lalit Behera <lalit143.er@gmail.com>
2019-02-14 09:49:51 +0530

    Handler is used to communicate with UI thread--handler post()  method demo

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,11 +2,35 @@ package com.lkb.baseandroidproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var mHandler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mHandler = Handler()
+        mStartButton.setOnClickListener{
+            startProgress();
+        }
+    }
+
+    private fun startProgress() {
+        //New thread to perform background operation
+        Thread(Runnable {
+            for (i in 0..30) {
+                try {
+                    Thread.sleep(500)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+                //Update the value background thread to UI thread
+                mHandler!!.post { mProgressBar.progress = i }
+            }
+        }).start()
     }
 }
