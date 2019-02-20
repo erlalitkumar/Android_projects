e2361cb819fc556faace1128531619d6d54a683e
Lalit Behera <lalit143.er@gmail.com>
2019-02-20 12:58:19 +0530

    Asynctask updating ui form doInBackground

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.lkb.baseandroidproject
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+        val task = MyAsyncTask()
+        task.execute()
+    }
+
+    inner class MyAsyncTask:AsyncTask<Void,Void,String>() {
+        override fun doInBackground(vararg params: Void?): String {
+            //Thread.sleep(3000)
+            textView.text ="From AsyncTask"
+            return "hello"
+        }
     }
 }
