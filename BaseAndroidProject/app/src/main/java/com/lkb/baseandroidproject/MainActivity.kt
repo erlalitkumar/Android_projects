2ac611164b8579231a7a0c8cdffd09f1ae8ffc16
Lalit Behera <lalit143.er@gmail.com>
2019-02-20 14:25:51 +0530

    call back and logfile added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,10 +3,11 @@ package com.lkb.baseandroidproject
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView:TextView
+    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +17,23 @@ class MainActivity : AppCompatActivity() {
         task.execute()
     }
 
-    inner class MyAsyncTask:AsyncTask<Void,Void,String>() {
+    override fun onStart() {
+        super.onStart()
+        Log.w("MainActivity","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("MainActivity","onResume")
+    }
+
+    inner class MyAsyncTask : AsyncTask<Void, Void, String>() {
         override fun doInBackground(vararg params: Void?): String {
-            //Thread.sleep(3000)
-            textView.text ="From AsyncTask"
+            var count = 0;
+            while (true) {
+                textView.text = "From AsyncTask" + count++
+                Log.w("MainActivity","From AsyncTask" + count++)
+            }
             return "hello"
         }
     }
