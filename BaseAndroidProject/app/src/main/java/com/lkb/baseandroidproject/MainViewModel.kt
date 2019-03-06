3d19a5845ea966e22978bf490c4cb983193729d6
Lalit Behera <lalit143.er@gmail.com>
2019-03-06 10:20:36 +0530

    some chage

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainViewModel.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainViewModel.kt
@@ -5,7 +5,13 @@ import io.reactivex.Observable
 
 class MainViewModel : ViewModel() {
     fun getData(): Observable<String> {
-        return Observable.just("hello", "hi", "there")
+        //return Observable.just("hello", "hi", "there").delay(3000,TimeUnit.MILLISECONDS)
+        return Observable.range(1,100)
+            .flatMap {
+                Thread.sleep(2000)
+                Observable.just(it.toString())
+            }
+
     }
 
 }
