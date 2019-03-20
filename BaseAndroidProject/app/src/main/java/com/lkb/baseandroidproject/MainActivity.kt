279a366c2eceb87a52086c3dd27abe3a8c17ae26
Lalit Behera <lalit143.er@gmail.com>
2019-03-20 10:01:02 +0530

    media streaming initial code added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.lkb.baseandroidproject
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val url = "http://prclive1.listenon.in:9960/;" // your URL here
+        val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        }
     }
 }
