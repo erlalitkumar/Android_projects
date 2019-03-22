efab3f0ff3de1cf64ac75d3b18c3d7b07c88d005
Lalit Behera <lalit143.er@gmail.com>
2019-03-22 19:21:00 +0530

    code refactored

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,17 +1,68 @@
 package com.lkb.baseandroidproject
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
-import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        var TAG = "MainActivity"
+    }
+
+    private var mMusicService: MusicService? = null
+    private var mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.d(TAG, "Music service disconnected.")
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            mMusicService = (service as MusicService.LocalBinder).service
+            Log.d(TAG, "Music service Connected")
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this@MainActivity, MusicService::class.java)
-        startService(intent)
+        doBindService()
+
+        btnChannel.setOnClickListener {
+            var intent = Intent(this@MainActivity,MusicService::class.java)
+            intent.putExtra("channel","http://prclive1.listenon.in:9960/;")
+            startService(intent)
+        }
+
+        btnChannel3.setOnClickListener {
+            var intent = Intent(this@MainActivity,MusicService::class.java)
+            intent.putExtra("channel","https://prclive4.listenon.in/International")
+            startService(intent)
+        }
     }
+
+    private fun doBindService() {
+        startService(Intent(this@MainActivity, MusicService::class.java))
+        bindService(
+            Intent(
+                this@MainActivity, MusicService::class.java
+            ), mServiceConnection,
+            Context.BIND_AUTO_CREATE
+        )
+    }
+
+    private fun doUnbindService() {
+        unbindService(mServiceConnection)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        doUnbindService()
+    }
+
 }
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MusicService.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MusicService.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.*
@@ -11,12 +12,18 @@ import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.lkb.baseandroidproject.MyApplication.Companion.CHANNEL_ID
 
-class MusicService : Service() {
+
+class MusicService : Service(), MediaPlayer.OnPreparedListener {
+    override fun onPrepared(mp: MediaPlayer?) {
+        mp?.start()
+        Log.d(tag, "Track info is : ${mp?.trackInfo.toString()}")
+    }
 
     private var instance: MusicService? = null
     private val tag = "MusicService"
     private var serviceLooper: Looper? = null
     private var serviceHandler: ServiceHandler? = null
+    private var mediaPlayer: MediaPlayer? = null
 
     private fun isServiceRunning(): Boolean {
         return instance != null
@@ -34,18 +41,32 @@ class MusicService : Service() {
     private inner class ServiceHandler(looper: Looper) : Handler(looper) {
         @SuppressLint("MissingPermission")
         override fun handleMessage(msg: Message?) {
-            val url = "http://prclive1.listenon.in:9960/;" // your URL here
-            val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
-                setAudioStreamType(AudioManager.STREAM_MUSIC)
-                setDataSource(url)
-                prepare() // might take long! (for buffering, etc)
-                start()
+            Log.d(tag, "onHandle with the msg")
+            if (msg != null && msg.data.getString("url").isNotEmpty()) {
+                val url = msg?.data?.getString("url") // your URL here
+                Log.d(tag, "onHandle with the msg $url")
+                releaseMediaPlayer()
+                if (url != null) {
+                    createMediaPlayer(url)
+                }
             }
+
         }
     }
 
     override fun onCreate() {
-
+        val notificationIntent = Intent(this@MusicService, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, notificationIntent, 0
+        )
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("MyRadio")
+            .setContentText("Now Playing...")
+            .setSmallIcon(R.drawable.ic_baseline_play)
+            .setContentIntent(pendingIntent)
+            .build()
+        startForeground(1, notification)
         HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND).apply {
             start()
             // Get the HandlerThread's Looper and use it for our Handler
@@ -61,30 +82,59 @@ class MusicService : Service() {
 
     @SuppressLint("MissingPermission")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
-        val notificationIntent = Intent(this@MusicService, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0, notificationIntent, 0
-        )
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("MyRadio")
-            .setContentText("Now Playing...")
-            .setSmallIcon(R.drawable.ic_baseline_play)
-            .setContentIntent(pendingIntent)
-            .build()
-        startForeground(1, notification)
+        val channelUrl = intent?.getStringExtra("channel") ?: "http://prclive1.listenon.in:9960/;"
         Log.d(tag, "onStartCommand executed")
-        if (!isServiceRunning()) {
-            Log.d(tag, "onStartCommand executed with job");
-            serviceHandler?.obtainMessage()?.also { msg ->
-                msg.arg1 = startId
-                serviceHandler?.sendMessage(msg)
-            }
-            instance = this@MusicService
+        //if (!isServiceRunning()) {
+        Log.d(tag, "onStartCommand executed with job");
+        serviceHandler?.obtainMessage()?.also { msg ->
+            msg.arg1 = startId
+            val bundle = Bundle()
+            bundle.putString("url", channelUrl)
+            msg.data = bundle
+            serviceHandler?.sendMessage(msg)
         }
 
-        // If we get killed, after returning from here, restart
         return START_STICKY
+    }
+
+    fun refreshPlayBack() {
+
+    }
+
+    fun pausePlayBack() {
+        mediaPlayer?.pause()
+    }
+
+    fun resumePlayBack() {
+        mediaPlayer?.start()
+    }
+
+    fun createMediaPlayer(url: String) {
+        mediaPlayer = MediaPlayer().apply {
+            val attributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                .build()
+            setAudioAttributes(attributes)
+            setDataSource(url)
+            prepareAsync()
+            setOnPreparedListener(this@MusicService)
+        }
+    }
+
+    fun releaseMediaPlayer() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releaseMediaPlayer()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+        releaseMediaPlayer()
     }
 }
