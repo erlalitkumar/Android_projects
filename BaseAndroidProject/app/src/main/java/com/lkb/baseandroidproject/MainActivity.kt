238ee517f72b5067ed215e1b7e7339a327f942a8
Lalit Behera <lalit143.er@gmail.com>
2019-04-19 10:10:17 +0530

    ui scope created and launching new coroutine block

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.lkb.baseandroidproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
-
+    private val mJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + mJob)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        uiScope.launch {
+            delay(1_000)
+            Toast.makeText(this@MainActivity,"From coroutine",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mJob.cancel()
     }
 }
