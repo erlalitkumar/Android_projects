556df83d217208dea4c1b7bc0ec2e3b69dce2dad
Lalitkumar.Behera <lalitkumar.behera@172-11-17-82.lightspeed.irvnca.sbcglobal.net>
2019-05-08 16:33:18 +0530

    spannable text created

--- BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.lkb.demo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tvHello);
+        SpannableString spannableString = new SpannableString("Hello");
+        // setting the text as a Spannable
+        spannableString.setSpan(new
+                        ForegroundColorSpan(Color.RED),
+                0, 1,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setText(spannableString, TextView.BufferType.SPANNABLE);
     }
 }
