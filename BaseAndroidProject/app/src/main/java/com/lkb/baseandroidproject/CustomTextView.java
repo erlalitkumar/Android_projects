c8f50f0acbd3365f61436610fb6383c30ed46dcc
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-06-13 17:00:29 +0530

    android x migration

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/CustomTextView.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/CustomTextView.java
@@ -3,9 +3,9 @@ package com.lkb.baseandroidproject;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import androidx.appcompat.widget.AppCompatTextView;
 
-public class CustomTextView extends TextView {
+public class CustomTextView extends AppCompatTextView {
     public CustomTextView(Context context) {
         super(context);
     }
