92d9908aa58b3f41adaa43c82bd628b330ea11b9
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-06-15 13:47:07 +0530

    in and out animation added in toolbar

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lkb.baseandroidproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.animation.ValueAnimator
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,22 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val initialx = button.x
+        val initialx = searchImage.x
+        val xofClock = clockImage.x
+        val delta = initialx-xofClock
         button.setOnClickListener {
             isClicked = !isClicked
            if(isClicked){
-               val widthAnimator = ValueAnimator.ofFloat(initialx, 400f)
+               val widthAnimator = ValueAnimator.ofFloat(60f, 0f)
                widthAnimator.addUpdateListener { animation ->
                    val animatedValue = animation.animatedValue as Float
-                   button.translationX = animatedValue
+                   iconContainer.translationX = animatedValue
                    widthAnimator.duration = 500
                }
                widthAnimator.start()
            }else{
-               val widthAnimator = ValueAnimator.ofFloat(400f, initialx)
+               val widthAnimator = ValueAnimator.ofFloat(0f, 60f)
                widthAnimator.addUpdateListener { animation ->
                    val animatedValue = animation.animatedValue as Float
-                   button.translationX = animatedValue
+                   iconContainer.translationX = animatedValue
                    widthAnimator.duration = 500
                }
                widthAnimator.start()
