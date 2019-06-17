fac325d4653513a78d0fc853b8dacbc00b972915
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-06-17 15:26:11 +0530

    Recyclerview scroll listener added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,8 +3,11 @@ package com.lkb.baseandroidproject
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,6 +16,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count =0
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                count += dy
+                Toast.makeText(this@MainActivity, "$dx and $count", Toast.LENGTH_SHORT).show()
+                if(dy>0){
+                    inAndOutLinearAnimation(60f, 0f)
+                }else{
+                    inAndOutLinearAnimation(0f, 60f)
+                }
+            }
+
+        })
         button.setOnClickListener {
             isClicked = !isClicked
             if (isClicked) {
@@ -49,9 +65,9 @@ class MainActivity : AppCompatActivity() {
         widthAnimator.addUpdateListener { animation ->
             val animatedValue = animation.animatedValue as Float
             iconContainer.translationX = animatedValue
-            Log.d("ANIM"," animated value = $animatedValue")
+            Log.d("ANIM", " animated value = $animatedValue")
             widthAnimator.duration = 400
-            if(!iconContainer.isInLayout){
+            if (!iconContainer.isInLayout) {
                 iconContainer.requestLayout()
             }
 
