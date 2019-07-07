05182069b22f50bbab8cb97df75e15fa66b1a5ba
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-07-07 13:51:17 +0530

    file read write android internal storeate priveate mode

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.lkb.baseandroidproject
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val filename = "hello.txt"
+        val fileContents = "Hello world!"
+        writeFile(filename,fileContents)
+        val stringBuilder: StringBuilder = readFile(filename)
+        Log.d("File",stringBuilder.toString())
+    }
+
+    private fun writeFile(filename: String,fileContents:String) {
+        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+            Log.d("File", "Success")
+        }
+    }
+
+    private fun readFile(filename: String): StringBuilder {
+        var fileInputStream: FileInputStream? = null
+        fileInputStream = openFileInput(filename)
+        var inputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader = BufferedReader(inputStreamReader)
+        val stringBuilder: StringBuilder = StringBuilder()
+        var text: String? = null
+        while ({ text = bufferedReader.readLine(); text }() != null) {
+            stringBuilder.append(text)
+        }
+        return stringBuilder
     }
 }
