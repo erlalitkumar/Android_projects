18d0b4fdb262a602a06ae0b75bd1c5cfd3938fb7
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-07-10 14:59:13 +0530

    alert dialog added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.lkb.baseandroidproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.DialogInterface
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            showAddItemDialog(this)
+        }
+    }
+    private fun showAddItemDialog(c: Context) {
+        val taskEditText = EditText(c)
+        val dialog = AlertDialog.Builder(c)
+            .setTitle("Add a new task")
+            .setMessage("What do you want to do next?")
+            .setView(taskEditText)
+            .setPositiveButton("Add",
+                DialogInterface.OnClickListener { dialog, which -> val task = taskEditText.text.toString() })
+            .setNegativeButton("Cancel", null)
+            .create()
+        dialog.show()
     }
 }
