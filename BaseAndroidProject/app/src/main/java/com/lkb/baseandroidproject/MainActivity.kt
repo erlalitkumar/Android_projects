02bfac0f97f18ea138c3b867a7721628c8190c43
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-11-27 14:13:10 +0530

    expandable list view demo

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,11 +2,55 @@ package com.lkb.baseandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.ExpandableListView
+import android.widget.ExpandableListAdapter
+
 
 class MainActivity : AppCompatActivity() {
+    var expandableListView: ExpandableListView? = null
+    var expandableListAdapter: ExpandableListAdapter? = null
+    var expandableListTitle: List<String>? = null
+    var expandableListDetail: HashMap<String, List<String>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        expandableListView = findViewById(R.id.expandableListView)
+        expandableListDetail = ExpandableListDataPump.getData()
+        expandableListTitle =
+            ArrayList<String>((expandableListDetail as java.util.HashMap<String, MutableList<String>>?)?.keys)
+        expandableListAdapter =
+            CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail)
+        expandableListView?.setAdapter(expandableListAdapter)
+        expandableListView?.setOnGroupExpandListener(ExpandableListView.OnGroupExpandListener { groupPosition ->
+            Toast.makeText(
+                applicationContext,
+                expandableListTitle?.get(groupPosition) + " List Expanded.",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+
+        expandableListView?.setOnGroupCollapseListener(ExpandableListView.OnGroupCollapseListener { groupPosition ->
+            Toast.makeText(
+                applicationContext,
+                expandableListTitle?.get(groupPosition) + " List Collapsed.",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+
+        expandableListView?.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+            Toast.makeText(
+                applicationContext,
+                expandableListTitle?.get(groupPosition)
+                        + " -> "
+                        + expandableListDetail?.get(
+                    expandableListTitle?.get(groupPosition)
+                )?.get(
+                    childPosition
+                ), Toast.LENGTH_SHORT
+            ).show()
+            false
+        }
     }
 }
---
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MyExpandableList.java
@@ -0,0 +1,25 @@
+package com.lkb.baseandroidproject;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ExpandableListView;
+
+public class MyExpandableList extends ExpandableListView {
+    public MyExpandableList(Context context) {
+        super(context);
+    }
+
+    public MyExpandableList(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyExpandableList(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+//    public MyExpandableList(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+
+
+}
