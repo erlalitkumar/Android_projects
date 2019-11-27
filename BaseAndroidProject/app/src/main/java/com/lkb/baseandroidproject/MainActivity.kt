8e37350457593ba285fb60e82ccede385507a30d
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-11-27 11:53:21 +0530

    glide code added

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -2,11 +2,17 @@ package com.lkb.baseandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(this)
+            .load("https://lalitbehera.github.io/dahlia.jpeg")
+            .override(300,400)
+            .into(imageView3);
     }
 }
