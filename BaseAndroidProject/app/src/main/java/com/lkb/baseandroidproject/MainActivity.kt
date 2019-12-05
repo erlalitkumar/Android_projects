db39abc626e43b0e32d9b43a3e4f9fdf1b49589c
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-05 20:53:01 +0530

    dagger: field injection used for framework classed we used the inject method with passing the instance

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -3,9 +3,10 @@ package com.lkb.baseandroidproject
 import android.os.Bundle
 import com.lkb.baseandroidproject.di.presentation.CarComponent
 import com.lkb.baseandroidproject.di.presentation.DaggerCarComponent
+import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
-    private var car: Car? = null
+    @Inject lateinit var car: Car
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,7 +14,8 @@ class MainActivity : BaseActivity() {
 //        var wheels = Wheels()
 //        var car = Car(engine,wheels)
         var component = DaggerCarComponent.create()
-        car = component.getCar()
+        //car = component.getCar()
+        component.inject(this)
         car?.drive()
     }
 }
--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/di/presentation/CarComponent.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/di/presentation/CarComponent.kt
@@ -1,9 +1,11 @@
 package com.lkb.baseandroidproject.di.presentation
 
 import com.lkb.baseandroidproject.Car
+import com.lkb.baseandroidproject.MainActivity
 import dagger.Component
 
 @Component
 interface CarComponent{
     fun getCar(): Car
+    fun inject(mainActivity: MainActivity)
 }
