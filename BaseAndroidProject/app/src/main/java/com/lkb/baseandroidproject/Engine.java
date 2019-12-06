09dcdc216f6baee89ccd0b83be8695b8b4b32a86
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-06 13:48:04 +0530

    dagger: code refactored

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/Engine.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/Engine.java
@@ -2,8 +2,6 @@ package com.lkb.baseandroidproject;
 
 import javax.inject.Inject;
 
-public class Engine {
-    @Inject
-    public Engine() {
-    }
+public interface Engine {
+    void start();
 }
