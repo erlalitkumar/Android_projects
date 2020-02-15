0336bec9e44750cc564183921fae9a3e593c3b20
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-02-15 20:35:43 +0530

    simple rxbinding added - on the button

--- BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.lkb.demo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.jakewharton.rxbinding3.view.RxView;
+import io.reactivex.disposables.Disposable;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.btnSubmit);
+        TextView t = findViewById(R.id.tvHello);
+        Disposable s = RxView.clicks(b).subscribe(unit -> t.setText("clicked"));
     }
 }
