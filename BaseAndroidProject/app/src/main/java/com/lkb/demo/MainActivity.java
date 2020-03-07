c872e9c14f13aa1d223c5961b38be1bb1d2b0e58
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-03-07 15:20:22 +0530

    account manager delete account and setting data in bundle

--- BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
@@ -2,8 +2,14 @@ package com.lkb.demo;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +20,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView helloText = findViewById(R.id.hello_txt);
+        Button button = findViewById(R.id.button);
         Account account = new Account("lalit143.er@gmail.com", getResources().getString(R.string.account_type));
 
         AccountManager am = AccountManager.get(this);
-        am.addAccountExplicitly(account, "123456", null);
+        Bundle bundle = new Bundle();
+        bundle.putString("access_token2", "ha-ha");
+        am.addAccountExplicitly(account, "123456", bundle);
         Account[] accounts = am.getAccountsByType("com.lkb.demo.app");
-        am.setUserData(account,"access_token","sjdkiee_dfkje_fjdkjdf_djff");
+        //am.setUserData(account, "access_token", "sjdkiee_dfkje_fjdkjdf_djff");
+        helloText.setText("account name is :" + accounts[0].name + " and access_token is: " + am.getUserData(account, "access_token2"));
         Log.d("LKB1", "account name is : " + accounts[0].name);
-        Log.d("LKB1", "access_toke is  : " + am.getUserData(account,"access_token"));
+        Log.d("LKB1", "access_toke is  : " + am.getUserData(account, "access_token2"));
+
+        button.setOnClickListener(v -> {
+            am.removeAccount(account,
+                    future -> {
+                        Toast.makeText(this, "account removed", Toast.LENGTH_SHORT).show();
+                    }, null);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
