c69e9acce9c6bbccd188168b681c3ae941b6ed94
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-03-07 15:41:05 +0530

    code refactored

--- BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
@@ -2,11 +2,8 @@ package com.lkb.demo;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
