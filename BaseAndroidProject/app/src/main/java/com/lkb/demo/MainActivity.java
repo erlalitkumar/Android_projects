bb8f4cf8f25e4e77a1a14b8569cc2f2b38751925
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-04-17 20:27:59 +0530

    layout complete

--- BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
+++ BaseAndroidProject/app/src/main/java/com/lkb/demo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.lkb.demo;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //applyEffects();
+    }
+
+    private void applyEffects() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        hideBottomNav();
+    }
+
+    private void hideBottomNav() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
