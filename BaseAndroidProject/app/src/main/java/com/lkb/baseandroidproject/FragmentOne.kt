df59932c6acb0eb512cee43da352b23da01fdc28
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-07-18 18:41:20 +0530

    gradle file changed - project migrated to android x

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/FragmentOne.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/FragmentOne.kt
@@ -2,11 +2,11 @@ package com.lkb.baseandroidproject
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_one.*
 
 class FragmentOne : Fragment() {
