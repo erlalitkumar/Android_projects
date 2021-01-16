daf0a07359abb1e9e098b13e7d054165a4a8b4b5
lalitbehera <lalitkumar.b@landmarkgroup.in>
2021-01-16 10:37:02 +0530

    realm db dependency added and gradle updated

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.lkb.baseandroidproject
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import androidx.lifecycle.ViewModelProviders
 import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var mViewModel: MainViewModel
@@ -17,17 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
-        textView.text = ""
-        disposable = mViewModel.getData()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.single())
-            .subscribe({
-                textView.text = it.toString();
-            },
-                { error -> textView.text = error.localizedMessage },
-                { textView.text = "completed" }
-            )
 
     }
 
