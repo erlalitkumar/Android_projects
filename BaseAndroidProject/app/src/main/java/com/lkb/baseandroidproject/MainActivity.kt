43486c365c6202e62fe4406dc903cccfd126ad0b
lalitbehera <lalitkumar.b@landmarkgroup.in>
2021-02-02 09:36:30 +0530

    new hilt branch created

--- BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
+++ BaseAndroidProject/app/src/main/java/com/lkb/baseandroidproject/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.lkb.baseandroidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var ss: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       this::ss.isInitialized
     }
 }
